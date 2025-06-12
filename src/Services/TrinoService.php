2fe398e406618332c776f9dcaeeca8ede654d6ee
VitaliyHrabovych <vitaliihr@softwareplanet.group>
2025-06-12 14:24:08 +0300

    Initial implementation for trino service + filter only get requests

--- src/Services/TrinoService.php
+++ src/Services/TrinoService.php
@@ -20,11 +20,40 @@ class TrinoService extends SqlDb
     public static function adaptConfig(array &$config)
     {
         $config['driver'] = 'trino';
+        if (!isset($config['odbc'])) {
+            $config['odbc'] = [];
+        }
+        foreach ($config as $key => $value) {
+            if (!isset($config['odbc'][$key])) {
+                $config['odbc'][$key] = $value;
+            }
+        }
         parent::adaptConfig($config);
     }
 
     public static function getDriverName()
     {
         return 'trino';
+    }
+
+    public function getApiDocInfo()
+    {
+        $base = parent::getApiDocInfo();
+
+        //For now, only keep the _table endpoint with GET requests to prove concept
+        $paths = [];
+        if (isset($base['paths'])) {
+            foreach ($base['paths'] as $path => $methods) {
+                if (str_contains($path, '_table')) {
+                    if (isset($methods['get'])) {
+                        $paths[$path]['get'] = $methods['get'];
+                    }
+                }
+            }
+        }
+        $base['paths'] = $paths;
+
+        $base['description'] = 'Trino service for connecting to Trino SQL endpoints.';
+        return $base;
     }
 }
