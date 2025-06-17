c6a6d61b46d60c459c8c97b4e332f63e29efb8a5
VitaliyHrabovych <vitaliihr@softwareplanet.group>
2025-06-17 15:06:54 +0300

    Setting up initial Trino Connector connection methods

--- src/Database/Connectors/TrinoConnector.php
+++ src/Database/Connectors/TrinoConnector.php
@@ -11,16 +11,24 @@ class TrinoConnector extends Connector implements ConnectorInterface
 {
     public function connect(array $config)
     {
-
-    }
-
-    public function createODBCConnection($dsn)
-    {
-
+        return $this->createConnection(
+            $this->getDsn($config),
+            $config,
+            $this->getOptions($config)
+        );
     }
 
     protected function getDsn(array $config)
     {
+        $cfg = $config['odbc'];
 
+        return "odbc:Driver={$cfg['driver_path']};"
+            . "Host={$cfg['host']};"
+            . "Port={$cfg['port']};"
+            . "AuthMech=0;"
+            . "UID={$cfg['username']};"
+            . "PWD={$cfg['password']};"
+            . "Catalog=memory;"
+            . "Schema=default";
     }
 }
